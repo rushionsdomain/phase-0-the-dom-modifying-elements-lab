5a89cc8d1eb6e85bd74a7fcf21142488c76d79f6
Rushion <rushionchegge@gmail.com>
2024-07-10 12:51:42 +0300

    edited js file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "FRANCIS is the champion";
+
+document.body.appendChild(newHeader);
