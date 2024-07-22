518209b1350ea01169e708801f6e28d6da65f2f2
Rushion <rushionchegge@gmail.com>
2024-07-22 11:17:26 +0300

    added an index.js file to meet requirements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader);
